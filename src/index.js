0da88b6bf915b3d8a114dc76d99fb728b064ee5f
Neematrashid <nymatrasheed05@gmail.com>
2023-09-30 00:31:23 +0300

    new commit

--- src/index.js
+++ src/index.js
@@ -1,42 +1,25 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
-});
-let listform = document.querySelector('#create-New-Task');
-listform.addEventListener('submit', function(e){
-  //this line adds an event of the 'listform' element
+//grabbing all the necessary DOM elements
+const tasklist = new taskList();
+
+const newtaskForm = document.getElementById('taskform');
+const taskList = document.getElementById('tasklist');
+const taskUl = document.getElementById('tasklistUl');
+//attach all event listeners
+const attachevents = () => (taskUl.innerHTML = tasklist.eventTasks);
+
+newtaskForm.addEventListener("submit", (e) => {
   e.preventDefault();
-  createToDo(e.target['new-task-description'].value);
-  //the code prevents the default behavior like
-  //(refreshing the page) and calls a function 
-  //called 'createToDo' with the value entered in the 
-  //'new-task-description' inputfield
+  taskList.Save-Task(Todotask.value);
+  // reset form
+  e.target.reset();
+  attachevents();
 });
 
-function createToDo(description){
-  let bttn = document.createElement('button')
-  bttn.addEventListener('click', Deleteitem)
-  bttn.textContent = 'x'
-  //this button is created inside the 'myToDolists'
-  //element and is added as an event listener to the 
-  //'click' event.
-  //the 'click' event is triggered when the user clicks on the 
-  //button.
-  let li_items = document.createElement('li');
-  li_items.textContent = description;
-  li_items.appendChild(bttn);
-  document.querySelector('#list').appendChild(li_items);
-  //the 'li_items' element is created inside the 'list'
-  //element and is added as an event listener to the 
-  //'click' event.
-  //the 'click' event is triggered when the user clicks on the 
-  //button.
-}
-
-function Deleteitem(e){
-  //this line adds an event of the 'listform' element
-  e.target.parentElement.remove();
-  //the code prevents the default behavior like
-  //(refreshing the page) and calls a function 
-  //called 'createToDo' with the value entered in the 
-  //'new-task-description' inputfield
-}
+taskUl.addEventListener("click", (e) => {
+  if (e.target.nodeName === "BUTTON") {
+    taskList.deleteTask(e.target.dataset.description);
+    attachevents();
+  }
+});
+});
